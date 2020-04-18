d5dc8b605e12d598d5378b21fccb0800f85c5f90
Tomasz Darmetko <isinlor@gmail.com>
2020-04-18 13:56:30 +0200

    Add simple setup

---
+++ common.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use Monolog\Logger;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpClient\HttpClient;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$logger = new Logger('Logger');
+
+$domCrawler = new Crawler();
+$filesystem = new Filesystem();
+
+$httpClient = HttpClient::create();
